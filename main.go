abb00f03f77b81b8e10ea0c1f168ccda8ea35ba3
nailo2c <nailo2c@gmail.com>
2020-02-22 00:10:51 +0800

    add initBoard

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "github.com/nsf/termbox-go"
 
+const boardLen = 4
+
 func main() {
 	err := termbox.Init()
 	if err != nil {
@@ -10,4 +12,13 @@ func main() {
 	defer termbox.Close()
 
 	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	board := initBoard(boardLen)
+}
+
+func initBoard(len int) [][]int {
+	board := make([][]int, len)
+	for i := range board {
+		board[i] = make([]int, len)
+	}
+	return board
 }
