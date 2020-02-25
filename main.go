78bdfd8dcdfab73437fd4b608cda965e0e85cddf
nailo2c <nailo2c@gmail.com>
2020-02-25 23:23:38 +0800

    complete the drawGameField function

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"math/rand"
 	"os"
+	"strings"
+	"time"
 
 	"github.com/nsf/termbox-go"
 )
@@ -32,6 +36,10 @@ func initBoard(len int) [][]int {
 
 func drawGameField(board [][]int) {
 	putNextNumber(board)
+	putNextNumber(board)
+	boardStartY := printTerminal(0, 0, []string{"Game 2048", ""})
+	boardEndY := drawBoard(0, boardStartY, board)
+	gameFieldEndY = printTerminal(0, boardEndY, []string{"Esc ←↑↓→", ""})
 }
 
 func putNextNumber(board [][]int) {
@@ -39,6 +47,10 @@ func putNextNumber(board [][]int) {
 	if len(emptyCells) <= 0 {
 		gameOver()
 	}
+	rndSrc := rand.NewSource(time.Now().UnixNano())
+	rnd := rand.New(rndSrc)
+	emptyCell := emptyCells[rnd.Intn(len(emptyCells))]
+	board[emptyCell/len(board)][emptyCell%len(board)] = 2
 }
 
 func findEmptyCells(board [][]int) []int {
@@ -66,6 +78,22 @@ func printTerminal(startX, startY int, strs []string) int {
 			termbox.SetCell(startX+x, startY+y, ch, termbox.ColorDefault, termbox.ColorDefault)
 		}
 	}
+	termbox.Flush()
+	return startY + len(strs)
+}
+
+func drawBoard(startX, startY int, board [][]int) int {
+	strs := []string{}
+	for _, row := range board {
+		var str string
+		for _, cell := range row {
+			str += fmt.Sprintf("%4d", cell)
+		}
+		str = strings.Replace(str, "0", ".", -1)
+		strs = append(strs, str, "")
+	}
+
+	printTerminal(0, startY, strs)
 	termbox.Flush()
 	return startY + len(strs)
 }
